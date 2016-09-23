20ded8fe1bbb1c08a864776fe2568dd1ebc04084
vovchek83 <vovchek83@gmail.com>
2016-09-23 23:57:38 +0300

    add model project

---
+++ IPTV.DataModel/EChannelGroup.cs
@@ -0,0 +1,18 @@
+﻿namespace IPTV.DataModel
+{
+    public enum EChannelGroup
+    {
+        Israel,
+        новости,
+        кино,
+        музыка,
+        познавательные,
+        детские,
+        развлекательные,
+        другие,
+        спорт,
+        HD,
+        взрослые,
+        None,
+    }
+}
---
+++ IPTV.DataModel/Models/ChannelModel.cs
@@ -0,0 +1,15 @@
+﻿namespace IPTV.DataModel.Models
+{
+    public class ChannelModel
+    {
+        public long Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Url { get; set; }
+
+        public string Icon { get; set; }
+
+        public EChannelGroup GroupName { get; set; }
+    }
+}
