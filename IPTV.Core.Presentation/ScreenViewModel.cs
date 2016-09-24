4f487d046c0669c945e342da9adf831f19cb97a7
vovchek83 <vovchek83@gmail.com>
2016-09-24 10:22:34 +0300

    add themes

---
+++ IPTV.Core.Presentation/ScreenViewModel.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.ComponentModel.Composition;
+using Caliburn.Micro;
+using IPTV.Logger;
+
+namespace IPTV.Core.Presentation
+{
+    public class ScreenViewModel : Screen, IDisposable
+    {
+        private bool _isBusy;
+
+        public bool IsBusy
+        {
+            get { return _isBusy; }
+            set
+            {
+                _isBusy = value;
+                NotifyOfPropertyChange();
+            }
+        }
+
+        [Import]
+        public ILogger Logger { get; set; }
+
+        [Import]
+        public IEventAggregator EventAggregator { get; set; }
+
+        /// <summary>
+        /// you shouldnt override this method, instead override the <see cref="DisposeImp"/>
+        /// </summary>
+        /// <param name="disposing"></param>
+        public void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                GC.SuppressFinalize(this);
+            }
+
+            DisposeImp();
+        }
+
+
+        /// <summary>
+        /// Add all your disposable logic here
+        /// </summary>
+        public virtual void DisposeImp()
+        {
+            //should beimplmented in derived classes
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+        }
+
+        ~ScreenViewModel()
+        {
+            Dispose(false);
+        }
+    }
+}
---
+++ IPTV.Core.Presentation/Window.cs
@@ -0,0 +1,46 @@
+﻿using System.Windows;
+using System.Windows.Input;
+
+namespace IPTV.Core.Presentation
+{
+    public class Window : System.Windows.Window
+    {
+        public Window()
+        {
+            CommandBindings.Add(new CommandBinding(SystemCommands.CloseWindowCommand, OnCloseWindow));
+            CommandBindings.Add(new CommandBinding(SystemCommands.MaximizeWindowCommand, OnMaximizeWindow, OnCanResizeWindow));
+            CommandBindings.Add(new CommandBinding(SystemCommands.MinimizeWindowCommand, OnMinimizeWindow, OnCanMinimizeWindow));
+            CommandBindings.Add(new CommandBinding(SystemCommands.RestoreWindowCommand, OnRestoreWindow, OnCanResizeWindow));
+        }
+
+        private void OnCanResizeWindow(object sender, CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = ResizeMode == ResizeMode.CanResize || ResizeMode == ResizeMode.CanResizeWithGrip;
+        }
+
+        private void OnCanMinimizeWindow(object sender, CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = ResizeMode != ResizeMode.NoResize;
+        }
+
+        private void OnCloseWindow(object target, ExecutedRoutedEventArgs e)
+        {
+            SystemCommands.CloseWindow(this);
+        }
+
+        private void OnMaximizeWindow(object target, ExecutedRoutedEventArgs e)
+        {
+            SystemCommands.MaximizeWindow(this);
+        }
+
+        private void OnMinimizeWindow(object target, ExecutedRoutedEventArgs e)
+        {
+            SystemCommands.MinimizeWindow(this);
+        }
+
+        private void OnRestoreWindow(object target, ExecutedRoutedEventArgs e)
+        {
+            SystemCommands.RestoreWindow(this);
+        }
+    }
+}
